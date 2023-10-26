1da9dd70e08c1aa0aa10da6881b5ba4e47b074d4
mohammedchakir <zerroukimedchakir@gmail.com>
2023-10-26 08:19:24 -0700

    building functions

--- factors_functions.c
+++ factors_functions.c
@@ -1,29 +1,35 @@
 #include <stdio.h>
-/*
-    Finds the smallest divisor, if any, of a given number `n`
-    Returns:
-        smallest divisor if found
-        0 if n is prime
-*/
-int trial_division(long int n){
-long int f;
 
-if (n%2 == 0){
-	printf("%lu=%lu*%i\n", n, n/2, 2);
-	return 0;
+/**
+  * trial_division: Finds the smallest divisor, if any, of a given number `n`
+  * @n: the number
+  * Returns:
+  * Smallest divisor if found
+  * 0 if n is prime
+  */
+int trial_division(long int n)
+{
+	long int f;
+
+	if (n % 2 == 0)
+	{
+		printf("%lu=%lu*%i\n", n, n / 2, 2);
+		return (0);
 }
 
 f = 3;
-while (f*f <= n)
+while (foo * bar <= n)
 {
-	if (n%f == 0){
-		printf("%lu=%lu*%lu\n", n, n/f, f);
-		return 0;
+	if (n % f == 0)
+	{
+		printf("%lu=%lu*%lu\n", n, n / f, f);
+		return (0);
 	}
-	else{
+	else
+	{
 		f += 2;
 	}
 }
 printf("%lu=%lu*%i\n", n, n, 1);
-return 0;
+return (0);
 }
